fed1300be53472b8c6857db5cbcbe6191e92c120
Scott Renz <scottrenz@scottrenz.com>
2019-07-11 16:34:52 -0400

    Card called axios, making function

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,7 +1,22 @@
 /* Step 1: using axios, send a GET request to the following URL 
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
-*/
+https://github.com/axios/axios
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+
+           */
+   axios.get('https://api.github.com/users/scottrenz')
+   .then (data => {
+       console.log('response',data)
+   }
+       
+       )
+   .catch (data => {
+           console.log('error',data)
+       }
+           
+           )
+           console.log('end') 
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -45,6 +60,46 @@ const followersArray = [];
 </div>
 
 */
+
+let cards = document.querySelectorAll('.card');
+
+cards.forEach(card => {
+  new card(article)
+}
+)
+
+function newcard(title,date,text) {
+  const divCard = document.createElement('div');
+  const cardImg = document.createElement('img');
+  const divInfo = document.createElement('div');
+  const h3 = document.createElement('h3');
+  const pName = document.createElement('p');
+  const pLoca = document.createElement('p');
+  const pProf = document.createElement('p');
+  const pFoll = document.createElement('p');
+  const pFing = document.createElement('p');
+  const pBio = document.createElement('p');
+  divCard.classList.add('card')
+  divInfo.classList.add('card-info')
+  h3.classList.add('name')
+  pName.classList.add('username')
+
+  h2.innerHTML = title;
+pDate.innerHTML = date;
+divcards.appendChild(divcard)
+divcard.appendChild(h2)
+divcard.appendChild(pDate)
+for (i=0; i<text.length; i++)
+{
+  divcard.appendChild(p[i])
+  if(typeof(text) != 'object')
+{break;}
+}
+divcard.appendChild(expandButton)
+
+return divcard
+}
+
 
 /* List of LS Instructors Github username's: 
   tetondan
---
+++ GitHubCard/index2.js
@@ -0,0 +1,7 @@
+          const script = document.querySelector('script')
+          let scriptString  = '    <script src="./GitHubCard/index2.js" async></script>';
+          script.insertAdjacentHTML('beforebegin', scriptString);
+
+          const script2 = document.querySelector('script')
+          let scriptString2  = '<script src="https://unpkg.com/axios/dist/axios.min.js" async></script>';
+          script2.insertAdjacentHTML('beforebegin', scriptString2);
